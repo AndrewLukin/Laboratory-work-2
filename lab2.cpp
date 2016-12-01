00f7d00fe78c92802206acaaf5963f30b1423e12
alexshapran-as <alexshapran.as@gmail.com>
2016-12-01 22:28:57 +0300

    init

--- lab2.cpp
+++ lab2.cpp
@@ -1,12 +1,13 @@
 /*
 Лабораторная работа №2
-Задание 1: Калькулятор со статическими переменными a и b
+Задание 2: Калькулятор с вводимыми с клавиатуры значениями a и b
 Алексей Шапран 
 Группа: ИУ8-14
 */
 
 #include <iostream>
 #include <conio.h>
+#include <cstdlib>
 using namespace std;
 
 double sum ( float a,  float b);
@@ -19,15 +20,19 @@ double sq ( float a );
 
 int main (void)
 {
-	 float a = 25.0;
-	 float b = 3.0;
+	float a = 0.0;
+	float b = 0.0;
 
-	cout << endl << "Calculator 0.1" << endl;
+	cout << endl << "Calculator 0.2";
+	cout << endl << endl << "Print a = "; 
+	cin >> a;
+	cout << "Print b = ";
+	cin >> b;
 	cout << endl << "a + b = " << sum(a,b) << endl;
 	cout << "a - b = " << sub(a,b) << endl;
 	cout << "a * b = " << mult(a,b) << endl;
-	if ( b != 0 ) cout << "a / b = " << div(a,b) << endl;
-		else cout << "b can not be zero";
+	if ( div(a,b) != false ) cout << "a / b = " << div(a,b) << endl;
+		else cout << "a / b = Error: it can not be divided by zero" << endl;
 	cout << "a^4 = " << pow(a) << endl;
 	cout << "sqrt(a) = " << sq(a) << endl;
 	
@@ -50,7 +55,8 @@ double mult ( float a,  float b)
 }
 double div ( float a,  float b)
 {
-	return (float)a / b;
+	if ( b != 0 ) return (float)a/b;
+		else return false;
 }
 double pow ( float a )
 {
@@ -82,7 +88,7 @@ double sq ( float a )
 
 	if ( a != 1 && a != 0 )
 	{
-		while ( abs(xn1 - xn) > 0.000001 )
+		while ( (xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001  )
 		{
 		 xn = xn1;
 		 xn1 = (float)(xn + (float)a/xn)/2;
