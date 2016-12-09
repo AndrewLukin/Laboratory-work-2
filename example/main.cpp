5d9eb2d3dd232f624fe8ee2ba722569ebc1dc982
Alex Shapran <shapran400@gmail.com>
2016-12-09 13:31:39 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -6,16 +6,21 @@ using namespace std;
 int main()
 {
     float a, b;
+    int* c = (int*)malloc(sizeof(int));
+	*c = 0;
     cin >> a;
     cin >> b;
     
     cout << sum( a, b ) << endl;
     cout << sub( a, b ) << endl;
     cout << mult( a, b ) << endl;
-    if ( div(a,b) != false ) cout << "a / b = " << div(a,b) << endl;
-        else cout << "a / b = Error: it can not be divided by zero" << endl; 
+    div(a,b,c);
+	if ( *c == 0 ) cout << "a / b = " << div(a,b,c) << endl;
+		else cout << "a / b = Error: it can not be divided by zero" << endl;
     cout << pow( a ) << endl;
-    cout << sq( a ) << endl;
+   sq(a,c);
+	if ( *c == 0 ) cout << "sqrt(a) = " << sq(a,c) << endl;
+		else cout << "sqrt(a) = Error: root of a negative number does not exist!" << endl;
     
     return 0;
 }
