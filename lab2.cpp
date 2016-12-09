c84ae37824625232da9f5b12b906ab252092ab65
Alex Shapran <shapran400@gmail.com>
2016-12-09 13:28:10 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -13,16 +13,17 @@ using namespace std;
 double sum ( float a,  float b);
 double sub ( float a,  float b);
 double mult ( float a,  float b);
-double div ( float a,  float b);
+double div ( float a,  float b, int* c );
 double pow ( float a );
 double abs (float a);
-double sq ( float a );
+double sq ( float a, int* c );
 
 int main (void)
 {
 	float a = 0.0;
 	float b = 0.0;
-
+	int* c = (int*)malloc(sizeof(int));
+	*c = 0;
 	cout << endl << "Calculator 0.2";
 	cout << endl << endl << "Print a = "; 
 	cin >> a;
@@ -31,10 +32,13 @@ int main (void)
 	cout << endl << "a + b = " << sum(a,b) << endl;
 	cout << "a - b = " << sub(a,b) << endl;
 	cout << "a * b = " << mult(a,b) << endl;
-	if ( div(a,b) != false ) cout << "a / b = " << div(a,b) << endl;
+	div(a,b,c);
+	if ( *c == 0 ) cout << "a / b = " << div(a,b,c) << endl;
 		else cout << "a / b = Error: it can not be divided by zero" << endl;
 	cout << "a^4 = " << pow(a) << endl;
-	cout << "sqrt(a) = " << sq(a) << endl;
+	sq(a,c);
+	if ( *c == 0 ) cout << "sqrt(a) = " << sq(a,c) << endl;
+		else cout << "sqrt(a) = Error: root of a negative number does not exist!" << endl;
 	
 	cout << endl << "Press any button to exit the program" << endl;
 
@@ -53,10 +57,10 @@ double mult ( float a,  float b)
 {
 	return a * b;
 }
-double div ( float a,  float b)
+double div ( float a,  float b, int* c)
 {
 	if ( b != 0 ) return (float)a/b;
-		else return false;
+		else *c = 1;
 }
 double pow ( float a )
 {
@@ -81,12 +85,15 @@ double abs (float a)
 
 	return a;
 }
-double sq ( float a )
+double sq ( float a, int* c )
 {
 	float xn = 1.0;  
 	float xn1 = 2.0;
-
-	if ( a != 1 && a != 0 )
+	
+	if ( a >= 0 )
+	{
+		*c = 0;
+		if ( a != 1 && a != 0 )
 	{
 		while ( (xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001  )
 		{
@@ -98,7 +105,9 @@ double sq ( float a )
 		else if ( a == 1 ) xn1 = 1;
 			else if ( a == 0 ) xn1 = 0;
 				else cout << "error";
-
-
-	return xn1;
+				return xn1;
+	}
+	
+	else *c = 1;
+	
 }
