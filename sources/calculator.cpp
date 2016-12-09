fb4b247d4428bfc61892dd8e2d962185e4e53397
Alex Shapran <shapran400@gmail.com>
2016-12-09 13:47:34 +0300

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -10,10 +10,10 @@ double mult ( float a,  float b)
 {
 	return a * b;
 }
-double div ( float a,  float b)
+double div ( float a,  float b, int* c)
 {
 	if ( b != 0 ) return (float)a/b;
-		else return false;
+		else *c = 1;
 }
 double pow ( float a )
 {
@@ -31,12 +31,22 @@ double pow ( float a )
 	
 	return (double) a;
 }
-double sq ( float a )
+double abs (float a)
+{
+	if ( a >= 0 ) a = a; 
+		else a = -a;
+
+	return a;
+}
+double sq ( float a, int* c )
 {
 	float xn = 1.0;  
 	float xn1 = 2.0;
-
-	if ( a != 1 && a != 0 )
+	
+	if ( a >= 0 )
+	{
+		*c = 0;
+		if ( a != 1 && a != 0 )
 	{
 		while ( (xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001  )
 		{
@@ -47,7 +57,10 @@ double sq ( float a )
 	}
 		else if ( a == 1 ) xn1 = 1;
 			else if ( a == 0 ) xn1 = 0;
-
-
-	return xn1;
+				else cout << "error";
+				return xn1;
+	}
+	
+	else *c = 1;
+	
 }
