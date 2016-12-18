d5f0e185c63c94236ced237bcdeb49bf03498ee3
Alex Shapran <shapran400@gmail.com>
2016-12-19 00:21:37 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -26,13 +26,13 @@ SCENARIO("calculator mult", "[mult]") {
 	REQUIRE( rv == 9.0 );
 }
 SCENARIO("calculator div", "[div]") {
-    float a = 9.0;
-    float b = 1.0;
+    float a = 1.0;
+    float b = 0.0;
     int* c = (int*)malloc(sizeof(int));
     
     double rv = div( a, b, c );
     
-	REQUIRE( rv == 9.0 );
+	REQUIRE( rv == nullptr );
 }
 SCENARIO("calculator pow", "[pow]") {
     float a = 2.0;
@@ -46,11 +46,11 @@ SCENARIO("calculator pow", "[pow]") {
 	REQUIRE( rv == 0.5 );
 }
 SCENARIO("calculator sq", "[sq]") {
-    float a = 9.0;
+    float a = -9.0;
     float b = 1.0;
     int* c = (int*)malloc(sizeof(int));
     
     double rv = sq( a, c );
     
-	REQUIRE( rv == 3.0 );
+	REQUIRE( rv == nullptr );
 }
