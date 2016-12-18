d9b5a20096ab0f4a0d1501d2cb6cf9f3abe66f4a
Alex Shapran <shapran400@gmail.com>
2016-12-18 23:51:34 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -9,14 +9,6 @@ SCENARIO("calculator sum", "[sum]") {
     
 	REQUIRE( rv == 10.0 );
 }
-SCENARIO("calculator sum", "[sum]") {
-    float a = 10.0;
-    float b = 1.0;
-    
-    double rv = sum( a, b );
-    
-	REQUIRE( rv == 11.0 );
-}
 SCENARIO("calculator sub", "[sub]") {
     float a = 9.0;
     float b = 1.0;
